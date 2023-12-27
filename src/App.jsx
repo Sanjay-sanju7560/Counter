97135cc56c1e22fc1925d31ba83322b59ab17156
Sanjay-ZAN <sanjaysanju7560@gmail.com>
2023-12-27 12:15:50 +0530

    'first'

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Counter from './components/Counter'
+
+function App() {
+
+
+  return (
+    <div className='w-100 d-flex justify-content-center aligh-items-center '  style={{height:'100%'}}>
+    <Counter/>
+    </div>
+  )
+}
+
+export default App
